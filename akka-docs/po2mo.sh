af6d0efe4a46441829e26cc61dda6714eb496e52
rkyymmt <rkyymmt@gmail.com>
2012-04-16 19:12:42 +0900

    add shell script for ja build.

---
+++ akka-docs/po2mo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+msgfmt translations/ja/po/$1.po -o translations/ja/LC_MESSAGES/$1.mo
---
+++ akka-docs/pot2po.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+msginit --locale=ja --input=translations/pots/$1.pot --output=translations/ja/po/$1.po
